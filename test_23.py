d0e5d28dcd9c50ba7de4b840562510d14b559141
Julian41-Hub <reiter-julian@web.de>
2025-11-27 17:53:55 +0100

    haölödö

---
+++ test_23.py
@@ -0,0 +1,9 @@
+name = input("Wie heißt du? ")
+age = int(input("Wie alt bist du? "))
+
+print(f"Hallo {name}!")
+
+if age >= 18:
+    print("Du bist volljährig.")
+else:
+    print("Du bist minderjährig.")
