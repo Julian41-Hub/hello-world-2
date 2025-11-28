5cf0b4a67c1dffd3584f2600d7385fc3d3b5338b
Julian41-Hub <reiter-julian@web.de>
2025-11-28 08:37:19 +0100

    new training

---
+++ test_24.py
@@ -0,0 +1,18 @@
+name = input ("wie heisst du?")
+age = int(input("wie alt bist ud?"))
+
+print(f"Hallo {name}!")
+
+if age >= 18:
+    print("Du bist volljährig.")
+else:
+    print("du bist minderjährig")
+
+if age >= 65:
+        print("Du bekommst seniorenrabatt.")
+
+if name == ("Julian"):
+       print ("willkommen admin")
+
+if age < 0 :
+        print("fehlermeldung")
